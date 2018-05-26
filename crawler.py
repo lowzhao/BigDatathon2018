73df0ad6ccfbea7de8434befd1d3d8a04d3af165
Kelvinyu98 <wukuwnyu@gmail.com>
2018-05-26 17:08:38 +0800

    nygnhju

---
+++ crawler.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get(
+    "http://atu.hk/adm-grades.php?programme=JS1204&year=2017")
+soup = BeautifulSoup(response.content, 'html.parser')
+
+dse_score = soup.find_all('td')
+dse_score = [item.text for item in dse_score]
+
+dse_arr = []
+
+while(len(dse_score)>=0):
+    dse_arr.append(dse_score[0:10])
+    for items in dse_arr:
+        
+
+
